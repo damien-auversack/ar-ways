1e3e5195ea6a24fce18fec566ccd77ec67001f51
damien <damien.auversack@outlook.be>
2022-02-04 16:10:45 +0100

    Start ar project with angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './view/homepage/homepage.component';
+import { CubeComponent } from './cube/cube.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    CubeComponent
   ],
   imports: [
     BrowserModule
