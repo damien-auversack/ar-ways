35553a5c3d51f99f17fa61e831f249e5e54ccc1c
damien <damien.auversack@outlook.be>
2022-02-09 14:06:03 +0100

    build

--- src/app/view/ar-scene/ar-scene.component.ts
+++ src/app/view/ar-scene/ar-scene.component.ts
@@ -28,27 +28,51 @@ export class ArSceneComponent implements OnInit, AfterViewInit {
   }
 
   initObjectsInMap(arrow: THREE.Group) {
-    let positions = [{x: 0.04034972786903381, y: -0.18314682021737097, z: -0.5702444970607757},
-      {x: 0.08605100065469742, y: 0.0021906256675720236, z: -1.2070549786090852},
-      {x: 0.07535369955003261, y: -0.049116116389632224, z: -1.8304659843444824}];
+    // let positions = [{x: 0.04034972786903381, y: -0.18314682021737097, z: -0.5702444970607757},
+    //   {x: 0.08605100065469742, y: 0.0021906256675720236, z: -1.2070549786090852},
+    //   {x: 0.07535369955003261, y: -0.049116116389632224, z: -1.8304659843444824}];
 
-    for(let elt of positions) {
+    let positionMurRouge = [
+      {x:0.09322841465473175,y:-0.005425304174423218,z:-0.7347625017166137},
+      {x:0.20034076720476152,y:-0.09324789047241211,z:-1.572834587097168},
+      {x:0.29266699999570844,y:0.04388910830020906,z:-2.747046911716461},
+      {x:0.3848316490650177,y:-0.01860302090644836,z:-3.846685492992401},
+      {x:0.5440651148557663,y:0.09806131720542907,z:-5.052800667285919},
+      {x:0.9403045296669006,y:0.10299928188323976,z:-6.280323624610901},
+      {x:1.1395349889993667,y:0.11774035692214965,z:-7.409593105316162},
+      {x:1.3196801573038102,y:0.18312819600105287,z:-8.565554308891297},
+      {x:1.5548635303974152,y:0.1606364965438843,z:-9.555590641498565},
+      {x:1.3769175291061402,y:0.09601531624794007,z:-10.622310471534728}
+    ];
+
+    let rotationMurRouge = [];
+
+    for (let i = 0; i < positionMurRouge.length; i++) {
       let cloneArrow = arrow.clone();
 
-      cloneArrow.children.forEach(child => {
-        child.rotation.set(0,1.57,0);
-      });
+      cloneArrow.position.set(positionMurRouge[i].x,positionMurRouge[i].y, positionMurRouge[i].z);
+      // cloneArrow.rotation.set(rotationMurRouge[i]._x,rotationMurRouge[i]._y, rotationMurRouge[i]._z, rotationMurRouge[i]._order);
 
-      cloneArrow.position.set(elt.x,elt.y, elt.z);
-
-       this.scene.add(cloneArrow);
+      this.scene.add(cloneArrow);
     }
+
+    // for(let elt of positionMurRouge) {
+    //   let cloneArrow = arrow.clone();
+    //
+    //   // cloneArrow.children.forEach(child => {
+    //   //   child.rotation.set(0,1.57,0);
+    //   // });
+    //
+    //   cloneArrow.position.set(elt.x,elt.y, elt.z);
+    //
+    //    this.scene.add(cloneArrow);
+    // }
   }
 
   loadObj() {
     return new Promise<THREE.Group>(resolve => {
       let objLoader = new OBJLoader;
-      objLoader.load('assets/arrow.obj', (object) => {
+      objLoader.load('assets/mark.obj', (object) => {
         resolve(object);
       });
 
@@ -68,6 +92,7 @@ export class ArSceneComponent implements OnInit, AfterViewInit {
 
     const light = new THREE.HemisphereLight(0xffffff, 0xbbbbff, 1);
     light.position.set(0.5, 1, 0.25);
+
     this.scene.add(light);
 
     this.renderer = new THREE.WebGLRenderer({antialias: true, alpha: true});
@@ -80,21 +105,37 @@ export class ArSceneComponent implements OnInit, AfterViewInit {
     const onSelect = () => {
       let cloneArrow = arrow.clone();
       cloneArrow.children.forEach(child => {
-        child.rotation.set(0,0.5*Math.PI,0.1*Math.PI);
+        child.rotation.set(0.005*Math.PI,Math.PI/2,0);
       });
 
-      cloneArrow.position.set(0, 0, -0.3).applyMatrix4(this.controller.matrixWorld);
+      cloneArrow.position.set(0, 0, -0.6).applyMatrix4(this.controller.matrixWorld);
 
       let matrix4 = this.controller.matrixWorld;
       cloneArrow.quaternion.setFromRotationMatrix(matrix4);
 
+      console.log(JSON.stringify(cloneArrow.position));
+      console.log(JSON.stringify(matrix4));
+
+      // let newMatrix4 = new Matrix4();
+
       // cloneArrow.quaternion.setFromRotationMatrix(matrix4);
+      // let vector1 = new THREE.Vector3();
+      // console.log(cloneArrow.getWorldPosition(vector1));
+      // let vector2 = new THREE.Vector3();
+      // console.log(cloneArrow.getWorldDirection(vector2));
 
       this.scene.add(cloneArrow);
     }
 
+    // let geometry = new THREE.CylinderGeometry(0, 0.05, 0.2, 32).rotateX(Math.PI / 2);
+    // const material = new THREE.MeshBasicMaterial( { color: 0xffff00 } );
+    // const circle = new THREE.Mesh( geometry, material );
+    // circle.position.set(0, 0, -0.3).applyMatrix4(this.controller.matrixWorld);
+    // this.scene.add( circle );
+
     this.controller = this.renderer.xr.getController(0);
     this.controller.addEventListener('select', onSelect);
+    this.scene.updateMatrixWorld
     this.scene.add(this.controller);
     let that = this;
     window.addEventListener('resize', () => {
